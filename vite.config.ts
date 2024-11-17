3398ad4eafef8deb41596579d9ec09bf1489f82b
Vighnesh Brahme <v8ganesh@gmail.com>
2024-11-17 22:33:03 +0530

    vercel deploy fix #2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ai-trip-planner-2/',
+  // base: '/ai-trip-planner-2/',
   plugins: [react()],
   resolve: {
     alias: {
