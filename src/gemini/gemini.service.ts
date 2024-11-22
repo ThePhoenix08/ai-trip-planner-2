de413ea044f1cac7d012fac437f9dd04c738548c
Vighnesh Brahme <v8ganesh@gmail.com>
2024-11-22 22:28:16 +0530

    env leak fix

--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -8,7 +8,7 @@ import {
   GenerationConfig,
 } from '@google/generative-ai';
 
-const apiKey = import.meta.env.VITE_APP_GEMINI_API_KEY;
+const apiKey = process.env.GEMINI_API_KEY;
 if (!apiKey) throw new Error('GEMINI_API_KEY is not set');
 const genAI = new GoogleGenerativeAI(apiKey);
 
